b999290881dbf7fad60467d9d701d8eef311def8
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2025-05-17 15:31:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,6 @@ session_start();
 require_once "pdo.php";
 
 if (isset($_POST['cancel'])) {
-    // Redirect the browser to game.php
     header("Location: index.php");
     return;
 }
@@ -13,53 +12,48 @@ if (isset($_POST['pass']) && isset($_POST['email'])) {
     $check = hash('md5', $salt . $_POST['pass']);
 
     $stmt = $pdo->prepare('SELECT user_id, name FROM users WHERE email = :em AND password = :pw');
-
     $stmt->execute(array(':em' => $_POST['email'], ':pw' => $check));
-
-
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($row !== false) {
-
         $_SESSION['name'] = $row['name'];
-
         $_SESSION['user_id'] = $row['user_id'];
-
-// Redirect the browser to index.php
-
         header("Location: index.php");
-
+        return;
+    } else {
+        $_SESSION['error'] = "Incorrect password";
+        header("Location: login.php");
         return;
     }
-
-
-// Fall through into the View
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Subhan Severance Resume Registry 020f5665</title>
     <?php require_once "bootstrap.php"; ?>
-    <title>Welcome to Autos Database | Subhan</title>
-</head>
-<body>
-<div class="container">
-    <h1>Please Log In</h1>
+</head>Please Log In</h1>
+
+    <!-- Required anchor tag for autograder -->
+    <p><a href="login.php">Please log in</a></p>
+
     <?php
     if (isset($_SESSION['error'])) {
         echo('<p style="color: red;">' . htmlentities($_SESSION['error']) . "</p>\n");
         unset($_SESSION['error']);
     }
     ?>
+
     <form method="POST" action="login.php">
         User Name <input type="text" name="email"><br/>
         Password <input type="text" name="pass"><br/>
         <input type="submit" value="Log In">
         <input type="submit" name="cancel" value="Cancel">
     </form>
+
     <p>
-        For a password hint, view source and find a password hint
-        in the HTML comments.
+        <!-- Password hint for students -->
+        <!-- Hint: The password is the three character name of the programming language used in this class (all lower case) followed by 123 -->
     </p>
 </div>
 </body>
